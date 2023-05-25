ffc68bf07eaeffcd36be3afe6a5d032c026cab45
Nathally <nathallyccd@gmail.com>
2023-05-25 16:20:10 -0300

    First commit

---
+++ src/app.controller.ts
@@ -0,0 +1,17 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Post()
+  getMessage(
+    @Body() message
+  ) {
+    return {
+      status: 'ok',
+      message
+    }
+  }
+}
---
+++ test/app.e2e-spec.ts
@@ -0,0 +1,28 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from './../src/app.module';
+
+describe('AppController (e2e)', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('/ (POST)', () => {
+    return request(app.getHttpServer())
+      .post('/')
+      .expect(200)
+      .then(response => {
+        expect(response.body).toMatchObject(
+          { status: 'ok' }
+        )
+      })
+  });
+});
