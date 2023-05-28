1a65729459e71932dd323d321c1941dc20436283
Nathally Souza <nathallyccd@gmail.com>
2023-05-28 14:52:30 -0300

    [FEAT]implements questionAnswer module

--- src/questionAnswer/questionAnswer.module.ts
+++ src/questionAnswer/questionAnswer.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
 import { QuestionAnswerService } from './questionAnswer.service';
+import { PrismaService } from '../client/prisma/prisma.service';
+import { QuestionAnswerRepository } from './repository/questionAnswer.repository';
+import { PrismaQuestionAnswerRepository } from './repository/prisma/prismaQuestionAnswer.repository';
 
 @Module({
-  providers: [QuestionAnswerService],
+  providers: [
+    QuestionAnswerService,
+    {
+      provide: QuestionAnswerRepository,
+      useClass: PrismaQuestionAnswerRepository
+    },
+    PrismaService
+  ],
 })
 export class QuestionAnswerModule {}
--- src/questionAnswer/questionAnswer.service.ts
+++ src/questionAnswer/questionAnswer.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { QuestionAnswer } from '@prisma/client';
+import { QuestionAnswerRepository } from './repository/questionAnswer.repository';
 
 @Injectable()
 export class QuestionAnswerService {
-  findAll() {
-    return `This action returns all questionAnswer`;
-  }
+
+  constructor(
+    private readonly questionAnswerRepository: QuestionAnswerRepository
+  ){}
+  getQuestionAnswers = async(questionId: string): Promise<QuestionAnswer[]> => this.questionAnswerRepository.getAnswersByQuestionId(questionId)
 }
