3c13bff64b7081d59e0c2791f9b12674573f9341
Nathally <nathallyccd@gmail.com>
2023-06-11 19:30:40 -0300

    [WIP]implements swagger

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,15 +2,18 @@ import { Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './service/auth.service';
 import { AuthRequestModel } from './model/authRequest.model';
+import { ApiCreatedResponse, ApiUnauthorizedResponse } from '@nestjs/swagger';
 
 @Controller()
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @ApiCreatedResponse({ schema: { example: { token: 'some-token' } } })
+  @ApiUnauthorizedResponse()
   @UseGuards(AuthGuard('local'))
   @Post()
-  async login(@Request() request: any) {
-    const data = request as AuthRequestModel;
+  async login(@Request() request: AuthRequestModel) {
+    const data = request;
     return this.authService.getToken(data.user);
   }
 }
--- src/company/dto/createCompanyRequest.dto.ts
+++ src/company/dto/createCompanyRequest.dto.ts
@@ -1,13 +1,23 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty } from 'class-validator';
 
 export class CreateCompanyRequestDTO {
+  @ApiProperty({
+    example: 'Company'
+  })
   @IsNotEmpty({ message: 'Required field' })
   name: string;
 
+  @ApiProperty({
+    example: 'company@email.com'
+  })
   @IsNotEmpty({ message: 'Required field' })
   @IsEmail({}, { message: 'Inform a valid email' })
   email: string;
 
+  @ApiProperty({
+    example: 'password'
+  })
   @IsNotEmpty({ message: 'Required field' })
   password: string;
 }
