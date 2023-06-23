56cc2c805550d400b24212c82321eb54f6897fb7
Nathally Souza <72207361+nathyts@users.noreply.github.com>
2023-06-23 17:44:46 -0300

    Update meta.controller.ts

--- src/meta/meta.controller.ts
+++ src/meta/meta.controller.ts
@@ -29,7 +29,7 @@ export class MetaController {
       const response = await axios({
         method: 'POST', // Required, HTTP method, a string, e.g. POST, GET
         url:
-          'https://graph.facebook.com/v12.0/' +
+          'https://graph.facebook.com/v17.0/' +
           req.body.entry[0].changes[0].value.metadata.phone_number_id +
           '/messages?access_token=' +
           process.env.WHATSAPP_TOKEN,
