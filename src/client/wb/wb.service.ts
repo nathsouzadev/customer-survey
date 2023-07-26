f614df7a8dec912986f8aa3323b0794d2bc388fb
Nathally Souza <nathallyccd@gmail.com>
2023-07-26 14:32:41 -0300

    [FEAT]catch error from meta

--- src/client/wb/wb.service.ts
+++ src/client/wb/wb.service.ts
@@ -12,51 +12,58 @@ export class WBService {
   sendMessage = async (
     messageData: MessageData | TemplateData,
   ): Promise<MessageSentModel> => {
-    const response = await axios({
-      method: 'POST',
-      url:
-        'https://graph.facebook.com/v17.0/' +
-        process.env.ADMIN_PHONE +
-        '/messages',
-      data: Object.keys(messageData).includes['template']
-        ? {
-            messaging_product: 'whatsapp',
-            to: messageData.receiver,
-            type: 'template',
-            template: {
-              name: messageData['template'],
-              language: {
-                code: 'pt_BR',
-              },
-              components: [
-                {
-                  type: 'body',
-                  parameters: messageData['parameters'],
+    try {
+      const response = await axios({
+        method: 'POST',
+        url:
+          'https://graph.facebook.com/v17.0/' +
+          process.env.ADMIN_PHONE +
+          '/messages',
+        data: Object.keys(messageData).includes['template']
+          ? {
+              messaging_product: 'whatsapp',
+              to: messageData.receiver,
+              type: 'template',
+              template: {
+                name: messageData['template'],
+                language: {
+                  code: 'pt_BR',
                 },
-              ],
+                components: [
+                  {
+                    type: 'body',
+                    parameters: messageData['parameters'],
+                  },
+                ],
+              },
+            }
+          : {
+              messaging_product: 'whatsapp',
+              to: messageData.receiver,
+              text: {
+                body: messageData['message'],
+              },
             },
-          }
-        : {
-            messaging_product: 'whatsapp',
-            to: messageData.receiver,
-            text: {
-              body: messageData['message'],
-            },
-          },
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${process.env.WHATSAPP_TOKEN}`,
-      },
-    });
-
-    this.logger.logger(
-      {
-        requestData: response.data,
-        message: 'Reply sent to user',
-      },
-      WBService.name,
-    );
-
-    return response.data;
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${process.env.WHATSAPP_TOKEN}`,
+        },
+      });
+  
+      this.logger.logger(
+        {
+          requestData: response.data,
+          message: 'Reply sent to user',
+        },
+        WBService.name,
+      );
+  
+      return response.data;
+    } catch (error) {
+      this.logger.errors(
+        error.message,
+        WBService.name,
+      );
+    }
   };
 }
